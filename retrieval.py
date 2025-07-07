60f0fc7c72c71e7dbb5f6b6dc1279abfab601116
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-07-07 10:26:47 -0600

    Changed model task

--- retrieval.py
+++ retrieval.py
@@ -18,7 +18,7 @@ def initialize_llm_qa_chain(model: str, temperature: int, max_tokens: int, top_k
         max_new_tokens=max_tokens,
         top_k=top_k,
         temperature=temperature,
-        task="text-generation",
+        task="conversational",
         provider="nebius",
         huggingfacehub_api_token=api_token
     )
