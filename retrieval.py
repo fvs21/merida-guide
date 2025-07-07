e41e8dc3099a68451cf2d20102c3461a7d036a02
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-07-07 10:32:11 -0600

    Changed provider

--- retrieval.py
+++ retrieval.py
@@ -18,8 +18,8 @@ def initialize_llm_qa_chain(model: str, temperature: int, max_tokens: int, top_k
         max_new_tokens=max_tokens,
         top_k=top_k,
         temperature=temperature,
-        task="conversational",
-        provider="nebius",
+        task="text-generation",
+        provider="fireworks-ai",
         huggingfacehub_api_token=api_token
     )
 
