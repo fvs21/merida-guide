e08366b603eca86dbd87396365c43720b8add8cf
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-07-06 18:45:18 -0600

    Changed ui

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ For more information on `huggingface_hub` Inference API support, please check th
 """
 client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 
-
 def respond(
     message,
     history: list[tuple[str, str]],
@@ -43,22 +42,7 @@ def respond(
 """
 For information on how to customize the ChatInterface, peruse the gradio docs: https://www.gradio.app/docs/chatinterface
 """
-demo = gr.ChatInterface(
-    respond,
-    additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
-    ],
-)
-
+demo = gr.ChatInterface(respond)
 
 if __name__ == "__main__":
     demo.launch()
